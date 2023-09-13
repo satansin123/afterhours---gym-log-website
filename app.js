f7e155de79d8208549cfa615ea820b665d6a0b0e
Neil <neil.gudwala@gmail.com>
2023-09-13 19:34:03 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC9K1IMpOjSb6G3W1pqsBEfXWpjn4RwOdw",
-  authDomain: "gym-log-30c7a.firebaseapp.com",
-  projectId: "gym-log-30c7a",
-  storageBucket: "gym-log-30c7a.appspot.com",
-  messagingSenderId: "919766881018",
-  appId: "1:919766881018:web:c6685844fed38f4052af43"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 import {getFirestore, collection, addDoc} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js"
-const db = getFirestore(app);
+const db = getFirestore();
 const dbRef = collection(db, "exercises");
 
 const addBtn = document.querySelector(".add-btn");
