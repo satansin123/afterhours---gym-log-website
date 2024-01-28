8547cd931ba2ccae97d02fb4bbdfe19edd56bfec
Neil <neil.gudwala@gmail.com>
2024-01-28 07:58:04 +0530

    good commit

---
+++ gym-log/gym/src/components/Content.js
@@ -0,0 +1,21 @@
+// Content.js
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div className="user-section">
+        <i className="fas fa-chevron-left"></i>
+        <div>
+          <i className="fas fa-user"></i>
+        </div>
+      </div>
+
+      <div className="cards-container">
+        {/* Cards go here */}
+      </div>
+    </div>
+  );
+};
+
+export default Content;
