9e8dc061e6dc89ac5fdc8b7e9687ca2ba1b58e8d
satansin123 <sanchaynts9422@gmail.com>
2024-01-28 09:13:29 +0530

    nutrition front end

--- gym/src/components/Card.js
+++ gym/src/components/Card.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ imageSrc, altText, title, to }) => {
   const cardStyle = {
-    width: '600px',
-    height: '350px', // Adjusted height for more space
+    width: '550px',
+    height: '300px', // Adjusted height for more space
     position: 'relative',
+    overflow: 'hidden', // Ensure that overflow content is hidden
+    transition: 'transform 0.3s', // Add a smooth transition effect
+    transform: 'translateY(0)', // Initial translation
   };
 
   const imageStyle = {
@@ -22,15 +25,19 @@ const Card = ({ imageSrc, altText, title, to }) => {
     transform: 'translateX(-50%)',
     color: 'white',
     textAlign: 'left',
-    
     width: '100%',
     fontWeight: 'bold',
     fontSize: '1.5em',
   };
 
+  // Define styles for the hover effect
+  const hoverStyles = {
+    transform: 'translateY(-8px)', // Move the card up slightly on hover
+  };
+
   return (
     <Link to={to}>
-      <div className="card" style={cardStyle}>
+      <div className="card" style={{ ...cardStyle, ...hoverStyles }}>
         <img src={imageSrc} alt={altText} className="card-image" style={imageStyle} />
         <div className="card-content" style={titleStyle}>
           <h3 className="card-title">{title}</h3>
